c837d8583bf7d60c19585775edd0b3baa027af43
safethecode <iam@sonjim.in>
2023-01-28 20:10:55 +0900

    왼쪽 패널 추가해서 레이아웃 만들기

--- src/components/Layouts/WorkspaceLayout.tsx
+++ src/components/Layouts/WorkspaceLayout.tsx
@@ -1,3 +1,13 @@
+import {
+  Component1Icon,
+  CounterClockwiseClockIcon,
+  FontSizeIcon,
+  LayersIcon,
+  Link2Icon,
+  Pencil2Icon,
+  RocketIcon,
+  TextIcon,
+} from '@radix-ui/react-icons';
 import { WorkspaceAsidePanel } from 'components/WorkspaceAsidePanel';
 import { styled } from 'styles/stitches';
 
@@ -6,8 +16,8 @@ interface WorkspaceLayoutProps {
 }
 
 const Wrapper = styled('main', {
-  display: 'flex',
   position: 'relative',
+  display: 'flex',
   width: '100%',
   height: '100%',
 });
@@ -15,17 +25,74 @@ const Wrapper = styled('main', {
 const Conatiner = styled('div', {
   position: 'relative',
   display: 'flex',
-  flex: 1,
   flexDirection: 'column',
+  flex: 1,
   width: '100%',
   height: '100%',
-  padding: '16px',
 });
+
+const LayoutMenuArr = [
+  {
+    title: '사업 관리',
+    items: [
+      {
+        name: '사업계획서 작성',
+        icon: <Pencil2Icon />,
+        url: '/workspace/business/plan',
+      },
+      {
+        name: '지표(KPI) 관리',
+        icon: <RocketIcon />,
+        url: '/workspace/business/kpi',
+      },
+    ],
+  },
+  {
+    title: '에셋 관리',
+    items: [
+      {
+        name: '상수 텍스트 추가/제거',
+        icon: <TextIcon />,
+        url: '/workspace/assets/constants',
+      },
+      {
+        name: '컴포넌트 관리',
+        icon: <Component1Icon />,
+        url: '/workspace/assets/components',
+      },
+      {
+        name: '다이어그램 관리',
+        icon: <LayersIcon />,
+        url: '/workspace/assets/diagrams',
+      },
+      {
+        name: '하이퍼링크 관리',
+        icon: <Link2Icon />,
+        url: '/workspace/assets/hyperlinks',
+      },
+    ],
+  },
+  {
+    title: '환경 설정',
+    items: [
+      {
+        name: '커스텀 폰트 관리',
+        icon: <FontSizeIcon />,
+        url: '/workspace/settings/fonts',
+      },
+      {
+        name: '문서 히스토리',
+        icon: <CounterClockwiseClockIcon />,
+        url: '/workspace/settings/history',
+      },
+    ],
+  },
+];
 
 export const WorkspaceLayout = ({ children }: WorkspaceLayoutProps) => {
   return (
     <Wrapper>
-      <WorkspaceAsidePanel />
+      <WorkspaceAsidePanel menu={LayoutMenuArr} />
       <Conatiner>{children}</Conatiner>
     </Wrapper>
   );
